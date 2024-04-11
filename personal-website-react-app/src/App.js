571bb553489d6862e7c9b103bba7c2fc1f3d2279
tillandert <116759551+tillandert@users.noreply.github.com>
2024-04-11 01:16:58 -0400

    Updated react app, may not wind up using react, we'll see.

--- personal-website-react-app/src/App.js
+++ personal-website-react-app/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <head>
+        <title>Taylor Tillander Personal Website</title>
+      </head>
+        <body>
+          <div className="margin-top: 50px; text-align: center;">
+              <h1>Welcome to Taylor Tillander's Personal Website!</h1>
+          </div>
+
+          <div className="margin-top: 20px; display: flex; justify-content: center; align-items: center;">
+              <button>Professional Experience</button>
+              <button>Projects</button>
+              <button>Education</button>
+              <button>Personal Life</button>
+          </div>
+
+          <div className="margin-top: 50px; text-align: center;">
+              <p>I am currently a third-year student studying computer engineering at the University of Florida. I am interested in many areas of computer science and electrical and computer engineering. Outside of school, I am a technical supervisor at the Stephen C. O'Connell Center, UF's basketball/volleball/gymnastics arena. In my free time, I enjoy working out, running, kickboxing, tennis, and videogames.</p>
+          </div>
+
+          <div className="margin-top: 50px; display: flex; justify-content: center; align-items: center; height: 1000px;">
+              <embed src="https://drive.google.com/file/d/1znxd-zL1JcuVz7zVPG7cfHsxg5BYJe2i/preview" width="800px" height="100%" />
+          </div>
+          <div className="margin-top: 50px; margin-bottom: 50px; text-align: center;">
+              <p>More to be added soon.</p>
+          </div>
+      </body>
     </div>
   );
 }
