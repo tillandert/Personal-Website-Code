a854734959c9aa76299b7465a08ae036ad818231
tillandert <tillandert@ufl.edu>
2024-12-12 22:47:01 -0500

    Update LinkedIn qr code

--- src/pages/Professional_Experience.jsx
+++ src/pages/Professional_Experience.jsx
@@ -15,8 +15,12 @@ const navigate = useNavigate();
 
             <div style={{ marginTop: '50px' }}>
                 <p>
-                    Outside of school, I am a technical supervisor at the Stephen C. O'Connell Center, UF's basketball/volleyball/gymnastics arena. I have also had two software engineering internships the past couple summers, first at Datamaxx doing primarily frontend development and then at Kratos doing scripting to run simulations.
+                    I currently work part time as a technical supervisor at the Stephen C. O'Connell Center, UF's basketball/volleyball/gymnastics arena. Next summer, I will be working as a software engineering intern at BNY Mellon. I have also had two software engineering internships the past couple summers, first at Datamaxx doing primarily frontend development and then at Kratos doing scripting to run simulations.
                 </p>
+            </div>
+
+            <div style={{ marginTop: '50px' }}>
+                <iframe src="https://drive.google.com/file/d/1znxd-zL1JcuVz7zVPG7cfHsxg5BYJe2i/preview" width="640" height="480" allow="autoplay"></iframe>
             </div>
 
             <div style={{ marginTop: '30px', textAlign: 'center' }}>
@@ -33,14 +37,6 @@ const navigate = useNavigate();
                         style={{ width: '200px', marginTop: '10px' }} 
                     />
                 </a>
-            </div>
-
-            <div style={{ marginTop: '50px' }}>
-                <iframe src="https://drive.google.com/file/d/1znxd-zL1JcuVz7zVPG7cfHsxg5BYJe2i/preview" width="640" height="480" allow="autoplay"></iframe>
-            </div>
-
-            <div style={{ marginTop: '50px' }}>
-                <p>More to be added soon.</p>
             </div>
         </div>
     );
